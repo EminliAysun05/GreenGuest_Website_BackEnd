1d20a521aeba4eb50863615b9a3f862e0bbc57ec
EminliAysun05 <aeminli232@gmail.com>
2025-05-23 23:00:11 +0400

    feat: social media links is done

--- GreenGuest_Web/obj/Debug/net8.0/GreenGuest_Web.AssemblyInfo.cs
+++ GreenGuest_Web/obj/Debug/net8.0/GreenGuest_Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GreenGuest_Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a12254d285142c57b5296a66c119355af248c58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b706eb6d3b8cd6a2cac8091a0fd4223bf909ee07")]
 [assembly: System.Reflection.AssemblyProductAttribute("GreenGuest_Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GreenGuest_Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
